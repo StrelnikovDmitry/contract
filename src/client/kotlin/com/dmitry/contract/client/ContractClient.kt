51de629e037e23ca47e2e9eaa42c2762bdfa5928
Dmitry_Strelnikov <formeazaz@gmail.com>
2026-07-25 21:57:46 +0700

    initial commit

---
+++ src/client/kotlin/com/dmitry/contract/client/ContractClient.kt
@@ -0,0 +1,9 @@
+package com.dmitry.contract.client
+
+import net.fabricmc.api.ClientModInitializer
+
+object ContractClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
