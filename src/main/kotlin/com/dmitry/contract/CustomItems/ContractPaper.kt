f58b42998778bef4aa32b931c54443ae3176ea7b
Dmitry_Strelnikov <formeazaz@gmail.com>
2026-07-28 11:28:48 +0700

    added tooltips for contract paper

--- src/main/kotlin/com/dmitry/contract/CustomItems/ContractPaper.kt
+++ src/main/kotlin/com/dmitry/contract/CustomItems/ContractPaper.kt
@@ -1,13 +1,21 @@
 package com.dmitry.contract.CustomItems
 
+import net.minecraft.client.item.TooltipContext
 import net.minecraft.entity.player.PlayerEntity
 import net.minecraft.item.Item
 import net.minecraft.item.ItemStack
+import net.minecraft.text.Text
 import net.minecraft.util.Hand
 import net.minecraft.util.TypedActionResult
 import net.minecraft.world.World
+import org.apache.logging.log4j.core.tools.picocli.CommandLine
 
 class ContractPaper(settings: Settings): Item(settings) {
+
+    override fun appendTooltip(stack: ItemStack, world: World?, tooltip: MutableList<Text>, context: TooltipContext) {
+        val data = stack.nbt?.get("data")
+        tooltip.add(Text.literal(data?.toString() ?: ""))
+    }
 
     //rmb use
     override fun use(world: World, user: PlayerEntity, hand: Hand): TypedActionResult<ItemStack> {
