a2adc40c3db135436b3ad23ed2467ccbd2b2f672
Dmitry_Strelnikov <formeazaz@gmail.com>
2026-07-30 00:38:13 +0700

    made new way of creating widgets
    TODO:
    make the button be on top of the texture

--- src/client/kotlin/com/dmitry/contract/client/screens/ContractScreen.kt
+++ src/client/kotlin/com/dmitry/contract/client/screens/ContractScreen.kt
@@ -2,7 +2,10 @@ package com.dmitry.contract.client.screens
 
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.ButtonWidget
+import net.minecraft.client.gui.widget.CyclingButtonWidget
 import net.minecraft.client.gui.widget.TextFieldWidget
+import net.minecraft.command.argument.IdentifierArgumentType.identifier
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 
@@ -11,40 +14,70 @@ class ContractScreen : Screen(Text.translatable("contract_screen")) {
     val textureHeight: Int = 180
     val texture = Identifier("contract", "textures/gui/contract_screen.png")
 
+    val buttonTextureSide: Int = 5
+    val buttonTextureCross = Identifier("contract", "textures/gui/cross.png")
+    val buttonTextureCheck = Identifier("contract", "textures/gui/check.png")
+
     val lineGap = 7
 
-    lateinit var contractTextField: TextFieldWidget
+    val maxLineAmount: Int = textureWidth/lineGap
+
+    val lines: MutableList<Pair<TextFieldWidget, ButtonWidget>> = mutableListOf()
 
     //screen should close after escape button is pressed
     override fun shouldCloseOnEsc(): Boolean = true
+
+    //build a line and a button near it
+    fun buildLine(i: Int): Pair<TextFieldWidget, CyclingButtonWidget<Boolean>>? {
+        if (i<maxLineAmount) {
+
+            //building a line
+            val x: Int = (width - textureWidth) / 2 + 16 + 12
+            val y: Int = 19 + (i * lineGap)
+
+            var contractTextField = TextFieldWidget(
+                textRenderer,
+                x, y,
+                textureWidth - 32, 5,
+                Text.translatable("contract_text_point$i")
+            ).apply {
+                setEditableColor(-12303292)
+                setUneditableColor(-13587920)
+                setMaxLength(((textureWidth - 32) / 6) - 2)
+                setDrawsBackground(false)
+            }
+
+            //building a button
+            val x_button: Int = (width - textureWidth) / 2 + 16
+
+            var checkBox = CyclingButtonWidget.onOffBuilder(Text.literal("x"),Text.literal("v"))
+                .build(
+                    x_button,
+                    y,
+                    buttonTextureSide,
+                    buttonTextureSide,
+                    Text.translatable("contract_checkbox$i")
+                )
+
+            return Pair(contractTextField, checkBox)
+        }
+        return null
+    }
 
     //initializing widgets
     override fun init() {
         super.init()
 
-        val x: Int = (width - textureWidth) / 2 + 16 + 12
-        val y: Int = 19
-
-        contractTextField = TextFieldWidget(
-            textRenderer,
-            x, y,
-            textureWidth - 32, 5,
-            Text.translatable("contract_text")
-        ).apply {
-            setEditableColor(-12303292)
-            setUneditableColor(-13587920)
-            setMaxLength(((textureWidth - 32) / 6) - 2)
+        val line = buildLine(0)
+        if (line!=null) {
+            addSelectableChild(line.first)
+            addDrawableChild(line.first)
+            addSelectableChild(line.second)
         }
-
-        addSelectableChild(contractTextField)
-        addDrawableChild(contractTextField)
     }
 
     //rendering screen
     override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
-        //rendering the screen on which all of that will be displayed
-        super.render(context, mouseX, mouseY, delta)
-
         //darkens the background
         this.renderBackground(context)
 
@@ -54,6 +87,9 @@ class ContractScreen : Screen(Text.translatable("contract_screen")) {
 
         //the game multiplies coordinates and texture sizes by gui scale, 3 is the default option in vanilla book.
         val y: Int = 3
+
+        //rendering the screen on which all of that will be displayed
+        super.render(context, mouseX, mouseY, delta)
 
         //drawing
         context.drawTexture(texture, x, y, 0f, 0f, textureWidth, textureHeight, textureWidth, textureHeight)
