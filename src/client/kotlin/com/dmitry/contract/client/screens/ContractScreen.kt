9c6e764b63dbc4173a6317328d582d7fbe7ebfbc
Dmitry_Strelnikov <formeazaz@gmail.com>
2026-07-29 16:01:25 +0700

    added text widget

--- src/client/kotlin/com/dmitry/contract/client/screens/ContractScreen.kt
+++ src/client/kotlin/com/dmitry/contract/client/screens/ContractScreen.kt
@@ -2,16 +2,37 @@ package com.dmitry.contract.client.screens
 
 import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.EditBoxWidget
 import net.minecraft.text.Text
 import net.minecraft.util.Identifier
 
-class ContractScreen: Screen(Text.translatable("contract_screen")) {
+class ContractScreen : Screen(Text.translatable("contract_screen")) {
     val textureWidth: Int = 146
     val textureHeight: Int = 180
     val texture = Identifier("contract", "textures/gui/contract_screen.png")
 
+    lateinit var contractTextField: EditBoxWidget
+
     //screen should close after escape button is pressed
     override fun shouldCloseOnEsc(): Boolean = true
+
+    //initializing widgets
+    override fun init() {
+        super.init()
+
+        val x: Int = (width - textureWidth) / 2 + 16
+        val y: Int = 16
+
+        contractTextField = EditBoxWidget(
+            textRenderer,
+            x, y,
+            textureWidth - 32, textureHeight - (2 * y),
+            Text.translatable("contract_text"),
+            Text.translatable("contract_text")
+        )
+
+        addDrawableChild(contractTextField)
+    }
 
     //rendering screen
     override fun render(context: DrawContext, mouseX: Int, mouseY: Int, delta: Float) {
